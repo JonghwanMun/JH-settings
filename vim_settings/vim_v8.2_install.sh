52b7a2fdda834b4a28001155884f5758db1a1b0e
jason.mun <jason.mun@kakaobrain.com>
2022-12-12 13:23:23 +0900

    update to install vim8.2; now vim github probides > v8.2, e.g., v9.0

--- vim_settings/vim_v8.2_install.sh
+++ vim_settings/vim_v8.2_install.sh
@@ -1,16 +1,17 @@
-# clones vim repository so we can build it from scratch
+# download vim (v8.2) so we can build it from scratch
 cd ~
-git clone https://github.com/vim/vim
-cd vim
-git pull && git fetch
+wget https://ftp.nluug.nl/pub/vim/unix/vim-8.2.tar.bz2
+tar xf vim-8.2.tar.bz2
+cd vim82
 
 # In case Vim was already installed. This can throw an error if not installed, 
 # it's the nromal behaviour. That's no need to worry about it
 cd src
 make distclean
-#cd ..
 
-sudo apt-get install libncurses5-dev libncursesw5-dev -y
+sudo apt install libncurses5-dev libncursesw5-dev libgtk2.0-dev \
+	libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev \
+	python2-dev python3-dev -y 
 
 export LDFLAGS="-fno-lto"
 ./configure --with-features=huge \
